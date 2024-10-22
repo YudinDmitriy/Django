fd467314ccb86790187ac6cf5414aa72f60da77e
Yudin Dmitriy <yudin01042002@yandex.ru>
2024-10-22 23:31:51 +0300

    22.2 задание 1

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 
     'catalog',
     'blog',
+    'users',
+
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    phone = models.CharField(max_length=30, verbose_name='телефон')
+    country = models.CharField(max_length=50, verbose_name='страна')
+    avatar = models.ImageField(upload_to="avatar/", verbose_name="аватар", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
